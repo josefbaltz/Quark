073eb3696ebd8202205f34fb40651a232d26112c
Josef Baltz <josef@baltz.net>
2019-01-17 16:17:36 +0000

    GCP Datastore Player Data

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	"os"
@@ -8,23 +9,33 @@ import (
 	"strings"
 	"syscall"
 
+	"cloud.google.com/go/datastore"
 	"github.com/bwmarrin/discordgo"
 )
 
 func init() {
 	flag.StringVar(&token, "t", "", "Discord API Token")
+	flag.StringVar(&projectID, "g", "", "Google Cloud Project ID")
 	flag.Parse()
 }
 
+var projectID string
 var token string
 var buffer = make([][]byte, 0)
 
+type Task struct {
+	Credits int
+	Level   int
+	Attack  int
+	Defense int
+}
+
 func main() {
 
-	if token == "" {
-		fmt.Println("Error 404 Token not found!")
+	if token == "" || projectID == "" {
+		fmt.Println("--Error--")
 		fmt.Println("Your start command should look like:")
-		fmt.Println("Quark -t <API Token>")
+		fmt.Println("Quark -t <Discord API Token> -g <GCP ProjectID>")
 		os.Exit(0)
 	}
 
@@ -32,6 +43,15 @@ func main() {
 	quark, err := discordgo.New("Bot " + token)
 	if err != nil {
 		fmt.Println("--Error--")
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	ctx := context.Background()
+	gcp, err := datastore.NewClient(ctx, projectID)
+	if err != nil {
+		fmt.Println("--Error--")
+		fmt.Println("Failed to create GCP client")
 		fmt.Println(err)
 		os.Exit(1)
 	}
@@ -53,6 +73,32 @@ func main() {
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
 	<-sc
 	quark.Close()
+}
+
+func populateUserGCP(ID string, session *discordgo.Session, event *discordgo.MessageCreate) {
+	ctx := context.Background()
+	gcp, err := datastore.NewClient(ctx, projectID)
+	if err != nil {
+		fmt.Println("--Error--")
+		fmt.Println("Failed to create GCP client")
+		fmt.Println(err)
+	}
+
+	taskKey := datastore.NameKey("User", ID, nil)
+
+	task := Task{
+		Attack:  0,
+		Defense: 0,
+		Credits: 10,
+		Level:   0,
+	}
+
+	if _, err := gcp.Put(ctx, taskKey, &task); err != nil {
+		fmt.Println("--Warning--")
+		fmt.Println("Couldn't Add user to GCP Datatstore")
+		fmt.Println(err)
+
+	}
 }
 
 func botConnected(session *discordgo.Session, event *discordgo.Ready) {
